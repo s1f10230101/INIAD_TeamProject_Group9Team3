385fade27581f7fd3238794fd971626f292391f5
Jaffy-Ou <oujaffy@icloud.com>
2025-09-08 00:40:53 +0900

    chore:いらないテーブルpostを削除した。

--- go-backend/db/migration/000003_create_reviews_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE reviews (
-    id UUID PRIMARY KEY,
-    spot_id UUID NOT NULL,
-    user_id UUID NOT NULL,
-    rating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
-    comment TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    FOREIGN KEY (spot_id) REFERENCES spot(id) ON DELETE CASCADE
-);
