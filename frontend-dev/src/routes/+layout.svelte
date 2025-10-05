cc8feacfb81b14bc38fe1a95d471f96f4cef01b6
s1f102301134 <s1f102301134@iniad.org>
2025-10-06 02:06:36 +0900

    frontend-ver1

--- frontend-dev/src/routes/+layout.svelte
+++ frontend-dev/src/routes/+layout.svelte
@@ -1,9 +1,19 @@
-<script lang="ts">
+<script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/icon2.png';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 	
 	let { children } = $props();
 </script>
+
+<Header />
+
+<main>
+
+</main>
+
+<Footer />
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
--- frontend-dev/src/routes/+page.svelte
+++ frontend-dev/src/routes/+page.svelte
@@ -1,2 +1,67 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import backgroundImage from '$lib/assets/back7.png'; 
+</script>
+
+<div class="full-screen-background" style="--background-url: url('{backgroundImage}')" >
+    <main>
+        <section class="vertical-text">
+            <h1>あなただけの旅行体験</h1>
+            <p>
+                本アプリは従来のガイドブックや<br />
+                SEOでは見つけられない<br />
+                まだ知られていない魅力的な場所と<br />
+                出会える旅行プラン検索アプリです
+            </p>
+        </section>
+
+        <section class="vertical-text">
+            <h1>隠れた名所を見つける</h1>
+            <p>
+                地元の事業者さんが登録した<br />
+                こだわりの観光施設や体験を<br />
+                豊富にそろえています
+            </p>
+        </section>
+
+        <section class="vertical-text">
+            <h1>あなただけの旅行体験</h1>
+            <p>
+                本アプリは従来のガイドブックや<br />
+                SEOでは見つけられない<br />
+                まだ知られていない魅力的な場所と<br />
+                出会える旅行プラン検索アプリです
+            </p>
+        </section>
+
+        <section class="vertical-text">
+            <h1>あなただけの旅行体験</h1>
+            <p>
+                本アプリは従来のガイドブックや<br />
+                SEOでは見つけられない<br />
+                まだ知られていない魅力的な場所と<br />
+                出会える旅行プラン検索アプリです
+            </p>
+        </section>
+
+
+
+    </main>
+</div>
+
+<style>
+    .full-screen-background{
+        background-size: contain;
+        /* background-position: center center; */
+        background-repeat: repeat;
+        background-image: var(--background-url); 
+    }
+
+    .vertical-text {
+        writing-mode: vertical-rl;
+        width: 10em; 
+        height: 100vh;
+        color: #5C4033;
+        text-shadow: black;
+    }
+
+</style>
