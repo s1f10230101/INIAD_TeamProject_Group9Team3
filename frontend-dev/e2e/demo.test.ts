6e355406680938ba1e2c832fc02574d8a33abe8a
s1f10230101 <s1f102301011@iniad.org>
2025-10-15 19:03:50 +0900

    ci: a

--- frontend-dev/e2e/demo.test.ts
+++ frontend-dev/e2e/demo.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('ルートページに0より多く、つまり1つ以上<h1>が存在することを確認する', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.locator('h1').count()).toBeGreaterThan(0);
+	await expect(await page.locator('h1').count()).toBeGreaterThan(0);
 });
