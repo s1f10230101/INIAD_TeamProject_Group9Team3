d524169d97eb78010e3be82b1869aa24d9cc5ba5
s1f10230101 <s1f102301011@iniad.org>
2025-11-13 02:10:49 +0900

    test: 逆にテストを増やしてみる

--- frontend-dev/e2e/facilityDitail.test.ts
+++ frontend-dev/e2e/facilityDitail.test.ts
@@ -3,15 +3,15 @@ import { expect, test } from "@playwright/test";
 test("施設詳細ページにモックデータが表示されることを確認する", async ({
   page,
 }) => {
-  // MSWのモックデータにレビューが存在する東京タワーのID
-  const facilityId = "c1b5c1c8-0b8f-4b1a-8b1a-0b8f4b1a8b1a";
+  // MSWのモックデータにレビューが存在する浅草寺のID
+  const facilityId = "d2c6d2d9-1c9g-5c2b-9c2b-1c9g5c2b9c2b";
   await page.goto(`/facilities/${facilityId}`);
 
-  // 同様に「東京タワー」も表示されることを確認します
-  const facility2 = page.getByText("東京タワー").first();
+  // 同様に「浅草寺」も表示されることを確認します
+  const facility2 = page.getByText("浅草寺");
   await expect(facility2).toBeVisible();
 
   // 住所の一部が表示されているかも確認できます
-  const address1 = page.getByText(/東京都港区芝公園４丁目２−８/);
+  const address1 = page.getByText(/東京都台東区浅草２丁目３−１/);
   await expect(address1).toBeVisible();
 });
---
+++ frontend-dev/e2e/facilityRegister.test.ts
@@ -0,0 +1,23 @@
+import { test } from "@playwright/test";
+
+test("施設登録ページにフォームが表示されることを確認する", async ({
+  page,
+}) => {
+  await page.goto(`/facilities/register`);
+
+  const nameInput = page.locator("#facilityName")
+  const locateInput = page.locator("#location")
+  const hours = page.locator("#hours")
+  const range = page.locator("#priceRange")
+  const description = page.locator("#description")
+  const submitButton = page.locator('button[type="submit"]');
+
+  await nameInput.fill("a")
+  await locateInput.fill("a")
+  await hours.fill("a")
+  await range.fill("a")
+  await description.fill("a")
+
+
+  await submitButton.click()
+});
